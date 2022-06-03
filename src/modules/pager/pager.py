2be10fc9727abb6b937f2506d0ec1b2f1cc9b532
Mitursky <mitursky.bogdan@yandex.ru>
2022-06-03 23:10:20 +0300

    Pager v1.2 (fix the html mode)

--- src/modules/pager/pager.py
+++ src/modules/pager/pager.py
@@ -11,12 +11,11 @@ from ..db.index import *
 from skimage.transform import resize
 from playwright.sync_api import sync_playwright
 import os
-
-
+import time
+import zlib
 IMGS_PATH = './src/modules/pager/screenshots'
 
 class Pager:   
-    
     def get_site_data(self, name='', url='', id='', type='img'):
         path = f"{IMGS_PATH}/{id}/{name}/"
         
@@ -28,6 +27,7 @@ class Pager:
             
         if not os.path.isdir(path+'/changes'):
             os.makedirs(path+'/changes')
+
         with sync_playwright() as p:
             browser = p.chromium.launch()
             page = browser.new_page()
@@ -39,6 +39,7 @@ class Pager:
 
             # swtich if type == 'img' or type == 'html'
             if type == 'html':
+                start_time = time.time()
                 print('html-check')
 
                 old_html = False
@@ -55,48 +56,53 @@ class Pager:
                 
                 # parse the html page to list of elements
                 elems = page.query_selector_all('div')
+                page_bounds = page.query_selector('body').bounding_box()
 
                 for elem in elems:
-                    # skip the main elements
-                  
                     # save the elements how text
-                    try:
-                        if elem  and elem.is_visible() and elem.inner_text() and elem.inner_html():
-                          
-                            html_data.write(str(elem.inner_text())+'/wathcer-elem-tag/')
-                    except:
-                        print('no HTML')
 
-                    html_data.close()
+                    if elem and elem.is_visible() and elem.inner_text() and elem.inner_html():
+                        html_data.write(str(zlib.compress(elem.inner_text().encode()))+'/wathcer-elem-tag/')
+
+
+                html_data.close()
                     # checking difference elements in elems and old_html
                     # make list of used elements
-                    used_list = ''
+                used_list = ''
                     
-                    # skip the main elements
-                   
-                    if old_html:
-                        for elem in elems:
-                            try:
-                                if elem.inner_text() and elem.is_visible() and elem.inner_text() not in old_html:
-                                    if elem.inner_html() not in used_list:
-                                       
-                                        elem.screenshot(path=f"{path}/changes/{changes_count}.png")
-                                        changes_count += 1
-                                        # save only 5 first elements
-                                        if changes_count > 5:
-                                            break
-                                        used_list+=('\n'+elem.inner_html())
-                                        print('new element')
-                                    else: 
-                                        print('element already used')
-                            except:
-                                print('no HTML')
-                        else:
-                            print('no new element')
+                    
+                iterations = 0
+                if old_html:
+                    for elem in elems:
+                        iterations += 1
+                        # counting progress iterations of elems
+                        print('Progress: ', str(iterations)+'/'+str(len(elems)))
+                        elem_bounds = elem.bounding_box()
+                        
+                        if not elem_bounds:
+                            continue
+                        try:
+                            if (elem_bounds['width'] * elem_bounds['height'] < page_bounds['height']*page_bounds['width']*0.6) and (not (elem_bounds['height'] / elem_bounds['width'] > 5 or elem_bounds['width'] / elem_bounds['height'] > 5)) and (elem_bounds['width'] * elem_bounds['height'] > 50*50) and elem.is_visible() and str(zlib.compress(elem.inner_text().encode())) not in old_html:
+                                if elem.inner_html() not in used_list:
+                                    elem.screenshot(path=f"{path}/changes/{changes_count}.png")
+                                    changes_count += 1
+                                    # save only 5 first elements
+                                    if changes_count > 10:
+                                        break
+                                    used_list += ('\n'+elem.inner_html())
+                                    print('new element')
+                                else: 
+                                    print('element already used')
+                        except:
+                            pass
                     else:
-                        print('no old element')
+                        pass
+                else:
+                    pass
                         
                     browser.close()
+                print('html-check done:', time.time() - start_time)
+
                 return changes_count
      
     def get_img(self, name='', id=''):
@@ -136,7 +142,5 @@ class Pager:
         if type == 'html':
             changes_count = self.get_site_data(name, url, id, type)
             return {"is_change": changes_count > 0, "name":name, "path":self.get_path(name,id), "status":"update", "changes_count":changes_count}
-
-    
 
 pager = Pager()
